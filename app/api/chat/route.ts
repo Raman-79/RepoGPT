d528275239004063aa739224e7e45dbb2769a805
Raman <raman@Ramans-MacBook-Air.local>
2024-12-17 14:43:48 +0530

    Fixing feature to add relavent files

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server';
+import {run }from '@/app/api/chat/init/route'
+
+export async function POST(req:NextRequest) {
+    const body = await req.json();
+    const {action , files} = body;
+    if(!action || !files){
+        return NextResponse.json({message:'ERROR NO ACTION OR BODY DEFINED'},{status:404});
+    }
+
+    //Call the LLM fetch the relavent files for the response
+    const initialResponse = await run("@cf/meta/llama-3-8b-instruct", {
+          messages: [
+            { role: "system", content: system_prompt_action },
+            { role: "user", content: JSON.stringify(data) },
+            { role: "assistant", content: agent_prompt_action  },
+          ],
+        });
+    
+    console.log("Initial response:", initialResponse);
+}
+
 export async function GET(req:NextRequest){
 
-    //1. Return the user answer of thier gitub repo 
-    //2. 
+
+    //Given a particular {ACTION}  and [relavent_files] return the LLM response
+
     return NextResponse.json({});
 }
---
+++ app/api/prompts/relavant-files.ts
@@ -0,0 +1,3 @@
+const consider_relavent_files = `You've will be provided with an array of files and directories of a project and an {ACTION} to perform which is sent by the user your task is to take 
+return an array of the relavent files you would need the content of to make perform the {ACTION} user asked you to do with reasonable answering for why did you consider those files for the {ACTION}.
+`

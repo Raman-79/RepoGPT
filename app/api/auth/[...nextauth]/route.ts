7289cae8d9a63d474f6375065ea72058180c5b33
Raman Singh <118994443+Raman-79@users.noreply.github.com>
2025-06-29 17:52:23 +0530

    Fix NextAuth and Theme Provider Errors

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,6 @@
 import { NEXT_AUTH } from "@/lib/auth"
 import NextAuth from "next-auth"
 
-const GET = async (req: Request) => {
-  const handler = NextAuth(NEXT_AUTH);
-  return handler(req);
-}
+const handler = NextAuth(NEXT_AUTH);
 
-const POST = async (req: Request) => {
-  const handler = NextAuth(NEXT_AUTH);
-  return handler(req);
-}
-
-export { GET, POST }
+export { handler as GET, handler as POST }
